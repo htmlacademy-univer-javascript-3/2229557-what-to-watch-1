c362901b853377d3cb022067195edbf4582cf51a
Гордиенко Артем Андреевич <gor.aa@skbkontur.ru>
2023-01-15 13:22:29 +0500

    feat: completed

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,5 +1,15 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
-import { getToken } from './token';
+import axios, {AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse} from 'axios';
+import {StatusCodes} from 'http-status-codes';
+import {toast} from 'react-toastify';
+import {getToken} from './token';
+
+const StatusCodeMapping: Record<number, boolean> = {
+  [StatusCodes.BAD_REQUEST]: true,
+  [StatusCodes.UNAUTHORIZED]: true,
+  [StatusCodes.NOT_FOUND]: true
+};
+
+const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping[response.status];
 
 export const createAPI = (): AxiosInstance =>
 {
@@ -20,5 +30,18 @@ export const createAPI = (): AxiosInstance =>
       return config;
     }
   );
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError) => {
+      if (error.response && shouldDisplayError(error.response)) {
+        toast.warn(error.message);
+      }
+
+      throw error;
+    }
+  );
   return api;
 };
+
+export const api = createAPI();
--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -2,9 +2,11 @@ import { createAction } from '@reduxjs/toolkit';
 import { Film } from '../types/film';
 import { AuthorizationStatus } from '../const';
 import { User } from '../types/user';
+import {ROUTES} from '../routes';
 
 export const changeGenre = createAction<string>('changeGenre');
 export const fillFilms = createAction<Film[]>('setFilms');
 export const setFilmsLoadedStatus = createAction<boolean>('setFilmsLoadedStatus');
 export const changeAuthorizationStatus = createAction<AuthorizationStatus>('changeAuthorizationStatus');
 export const setUser = createAction<User>('setUser');
+export const redirectToRoute = createAction<ROUTES | string>('redirectToRoute');
